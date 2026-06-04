6b10071616909cf3a5d3cfe551b613146d7e1d99
Kratikap23 <kratikap665@gmail.com>
2026-06-04 17:27:00 +0530

    Learn about bitwise operators

---
+++ bitwiseOp/bitwise.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    unsigned int x =-10;;
+    // int a= 4; int b=8;
+    // cout<< (a&b) << endl; // And operator
+    // cout <<(a|b)<<endl; // Or operator
+    // cout <<(a^b)<< endl; // Xor operator
+    // cout <<(10<<2)<<endl; // Left shift operator
+    // cout <<(10>>1) <<endl; // Right shift operator
+    // cout << sizeof(int)<<endl;
+    // cout << sizeof(long int)<<endl;
+    // cout << sizeof(long long int)<<endl;
+    // cout<< sizeof(short int )<<endl;
+    cout << x<< endl;
+    return 0;
+}
